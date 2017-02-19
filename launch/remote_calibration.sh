fb42bcd1095b5abef5dbb976a6d9a8031173043b
Tobias Allgeyer <uncxq@student.kit.edu>
2017-02-19 14:54:28 +0100

    Added files from old repo

---
+++ launch/remote_calibration.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# localhost is not a valid ROS_MASTER_URI for multiple machines
+export ROS_MASTER_URI="http://i61p134:11311"
+
+# workaround: A Bug causes failure to check for known_hosts
+export ROSLAUNCH_SSH_UNKNOWN=1
+
+
+roslaunch asr_calibration_tool_dome asr_calibration_tool_dome.launch 
---
+++ src/DataTracker.cpp
@@ -0,0 +1,200 @@
+/**
+
+Copyright (c) 2016, Heller Florian, Stöckle Patrick, Wittenbeck Valerij
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/*#include <image_transport/subscriber_filter.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <asr_msgs/AsrObject.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+class CalibrationTracker {
+public:
+        ros::NodeHandle nh_;
+
+        // Subscriptions
+        message_filters::Subscriber<sensor_msgs::JointState> ptu_sub_;
+        message_filters::Subscriber<asr_msgs::AsrObject> fob_sub_;
+        message_filters::Subscriber<geometry_msgs::PoseStamped> cbd_sub_;
+
+        std::string ptu_topic, fob_topic, cbd_topic, output_file_path;
+
+        typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::JointState, asr_msgs::AsrObject, geometry_msgs::PoseStamped> ApproximatePolicy;
+        typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
+        boost::shared_ptr<ApproximateSync> approximate_sync_;
+
+
+
+        CalibrationTracker(ros::NodeHandle nh_){
+            this->nh_ = nh_;
+            nh_.getParam("ptu_topic", ptu_topic);
+            nh_.getParam("fob_topic", fob_topic);
+            nh_.getParam("cbd_topic", cbd_topic);
+            nh_.getParam("output_file_path", output_file_path);
+
+            init_subscriber();
+        }
+
+        void syncCallback(const sensor_msgs::JointStateConstPtr& ptu_msg, const asr_msgs::AsrObjectConstPtr& fob_msg, const geometry_msgs::PoseStampedConstPtr& cbd_msg) {
+            ROS_DEBUG("synced Message recieved: %s", fob_msg->identifier.c_str());
+            if (!fob_msg->identifier.empty() && strcmp(fob_msg->identifier.c_str(), "tracker_left_raw") == 0) {
+                ROS_DEBUG("matching synced Message recieved");
+                std::stringstream ss;
+                ss << ptu_msg->position[0] << "; " << ptu_msg->position[1] << "; ";
+                ss << fob_msg->poseEstimation.pose.position.x << "; " << fob_msg->poseEstimation.pose.position.y << "; " << fob_msg->poseEstimation.pose.position.z << "; ";
+                ss << fob_msg->poseEstimation.pose.orientation.w << "; " << fob_msg->poseEstimation.pose.orientation.x << "; " << fob_msg->poseEstimation.pose.orientation.y << "; " << fob_msg->poseEstimation.pose.orientation.z << "; ";
+                ss << cbd_msg->pose.position.x << "; " << cbd_msg->pose.position.y << "; " << cbd_msg->pose.position.z << std::endl;
+
+                std::ofstream dataFile;
+                dataFile.open (output_file_path.c_str(),std::ios::app);
+                dataFile << ss.str();
+                dataFile.close();
+                ROS_INFO("data captured: %s",ss.str().c_str());
+
+            }
+
+        }
+
+
+        void init_subscriber() {
+
+                ptu_sub_ .subscribe(nh_, ptu_topic, 1);
+                fob_sub_ .subscribe(nh_, fob_topic, 1);
+                cbd_sub_ .subscribe(nh_, cbd_topic, 1);
+
+                int queue_size = 2000;
+                approximate_sync_.reset( new ApproximateSync(ApproximatePolicy(queue_size),ptu_sub_, fob_sub_, cbd_sub_) );
+                approximate_sync_->registerCallback(boost::bind(&CalibrationTracker::syncCallback, this, _1, _2, _3));
+        }
+
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "calibration_tracker");
+
+  ros::NodeHandle nh("~");
+  new ::CalibrationTracker(nh);
+  ros::spin();
+
+  return 0;
+}
+*/
+#include <ros/ros.h>
+#include <asr_msgs/AsrObject.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <posedetection_msgs/Object6DPose.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <boost/thread.hpp>
+
+
+class DataTracker {
+public:
+        ros::NodeHandle nh;
+
+        // Subscriptions
+        ros::Subscriber ptu_sub;
+        ros::Subscriber fob_sub;
+        ros::Subscriber cbd_sub;
+
+        std::string ptu_topic, fob_topic, cbd_topic, output_file_path;
+
+        double pan, tilt;
+        geometry_msgs::Pose fob_pose;
+        geometry_msgs::Pose cbd_pose;
+
+        int data_counter;
+
+        void ptuCB(const sensor_msgs::JointState& ptu_msg) {
+            ROS_DEBUG("ptu Message recieved");
+            pan = ptu_msg.position[0];
+            tilt = ptu_msg.position[1];
+        }
+
+        void fobCB(const asr_msgs::AsrObject& fob_msg) {
+            if (!fob_msg.identifier.empty() && strcmp(fob_msg.identifier.c_str(), "tracker_left_raw") == 0) {
+                ROS_DEBUG("fob message recieved");
+                geometry_msgs::PoseWithCovariance current_pose_with_c = fob_msg.sampledPoses.back();
+                fob_pose = current_pose_with_c.pose;
+            }
+        }
+        void cbdCB(const geometry_msgs::PoseStamped& cbd_msg) {
+            ROS_DEBUG("cbd message recieved");
+            ROS_INFO("cbd message recieved");
+            cbd_pose = cbd_msg.pose;
+        }
+
+        DataTracker(ros::NodeHandle nh){
+            this->nh = nh;
+            nh.getParam("ptu_topic", ptu_topic);
+            nh.getParam("fob_topic", fob_topic);
+            nh.getParam("cbd_topic", cbd_topic);
+            nh.getParam("output_file_path", output_file_path);
+
+            ptu_sub = nh.subscribe(ptu_topic, 1, &DataTracker::ptuCB, this);
+            fob_sub = nh.subscribe(fob_topic, 1, &DataTracker::fobCB, this);
+            cbd_sub = nh.subscribe(cbd_topic, 1, &DataTracker::cbdCB, this);
+            data_counter = 0;
+            boost::thread trackerThread(spin, this);
+            ros::spin();
+        }
+        static void spin(DataTracker* obj){
+            ROS_INFO("Capturing started.");
+            ROS_INFO("Enter 'e' to stop capturing.");
+            while (std::cin.get() != 'e'){
+               obj->write();
+            }
+            ROS_INFO("Capturing stopped.");
+        }
+
+
+        void write(){
+            std::stringstream ss;
+            ss << pan << "; " << tilt << "; ";
+            ss << fob_pose.position.x << "; " << fob_pose.position.y << "; " << fob_pose.position.z << "; ";
+            ss << fob_pose.orientation.w << "; " << fob_pose.orientation.x << "; " << fob_pose.orientation.y << "; " << fob_pose.orientation.z << "; ";
+            ss << cbd_pose.position.x << "; " << cbd_pose.position.y << "; " << cbd_pose.position.z << std::endl;
+            ROS_INFO("%s", ss.str().c_str());
+            std::ofstream dataFile;
+            dataFile.open (output_file_path.c_str(),std::ios::app);
+            dataFile << ss.str();
+            dataFile.close();
+            ROS_INFO("data captured %d", data_counter);
+            data_counter++;
+        }
+
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "calibration_tracker");
+
+  ros::NodeHandle nh("~");
+  new ::DataTracker(nh);
+  //ros::spin();
+
+  return 0;
+}
